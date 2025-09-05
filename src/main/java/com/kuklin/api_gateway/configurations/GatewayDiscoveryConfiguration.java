ce3af86f4896f35f51a75111bb07647c64885403
DanielK <saponembigas@gmail.com>
2025-09-05 23:48:48 +0700

    feat(et-1): api gateway settings

---
+++ src/main/java/com/kuklin/api_gateway/configurations/GatewayDiscoveryConfiguration.java
@@ -0,0 +1,26 @@
+package com.kuklin.api_gateway.configurations;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class GatewayDiscoveryConfiguration {
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(
+            ReactiveDiscoveryClient reactiveDiscoveryClient,
+            DiscoveryLocatorProperties locatorProperties) {
+
+        return new DiscoveryClientRouteDefinitionLocator(
+                reactiveDiscoveryClient,
+                locatorProperties
+        );
+    }
+
+
+}
